fc1bced223ee1e5a373514d33f597b23d434ba85
Marcin Kupiec <eustachy.kupiec@gmail.com>
2017-12-24 02:49:00 +0100

    feat: Disable eslint rule

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,3 +1,16 @@
 module.exports = {
-    "extends": "airbnb-base"
+  "extends": "airbnb-base",
+  "env": {
+    "browser": true,
+    "es6": true,
+    "mocha": true,
+    "jquery": true
+  },
+  "plugins": [
+    "mocha"
+  ],
+  "rules": {
+    "mocha/no-exclusive-tests": "error",
+    "array-callback-return": "off"
+  }
 };
